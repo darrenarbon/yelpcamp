987845f76193d2c35462ea58c0a5e5521a6db0a5
Darren Arbon <darren.arbon@gmail.com>
2018-04-12 10:08:34 +0100

    Modified to use remote database

--- app.js
+++ app.js
@@ -83,6 +83,6 @@ app.use("/campgrounds", campgroundRoutes);
 // start server //
 //==============//
 
-app.listen(3000, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
